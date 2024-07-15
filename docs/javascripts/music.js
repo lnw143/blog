595694264371285963d0f29c20343f6f3e33d8ae
lnw143 <lnwbkb143@163.com>
2024-07-16 07:36:46 +0800

    create music.js

---
+++ docs/javascripts/music.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+	$('body').append(`<meting-js
+		server="netease"
+		type="playlist"
+		id="12306622175"
+		style="position: fixed; bottom: 0px; right: 0px; width: 500px;"
+	>
+	</meting-js>`)
+
+	console.log("music.js loaded")
+}
