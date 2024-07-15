3031c8be800833c88d42d56a91f5b2bca596fbc5
lnw143 <lnwbkb143@163.com>
2024-07-15 22:12:47 +0800

    add music

---
+++ hooks/music.py
@@ -0,0 +1,11 @@
+def on_post_page(output, page, config):
+	# music_id = ['2078733133']
+	return output + '''
+<meting-js
+	server="netease"
+	type="playlist"
+	id="12306622175"
+	style="position: fixed; bottom: 0px; right: 0px; width: 500px;"
+>
+</meting-js>
+'''
